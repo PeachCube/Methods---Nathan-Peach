308dc9e4cf63cdc9e1693bbb44f3e2c1f17ecf54
PeachCube <nathanpeach500@live.com>
2023-09-19 16:09:57 -0300

    End of class; hud finished, basic methods set up

--- Methods - Nathan Peach/Program.cs
+++ Methods - Nathan Peach/Program.cs
@@ -8,15 +8,52 @@ namespace Methods___Nathan_Peach
 {
     internal class Program
     {
+        //Variables must be static in order for them to be accessed by static methods
+        //Variable declaration
+        static int health = 100;
+        static int gold = 0;
+        static int turn = 0;
+        static int potion = 1;
+        //Variable declaration
+
+
+
+
+
+
+
+        //Program always reads and executes "Main" first
         static void Main(string[] args)
         {
             //Calls the "Hud" method to print the Hud
             Hud();
+            TakeDamage(25);
+            Hud();
+
+        }
+        static void TakeDamage(int damage) 
+        {
+            health = health - damage;
         }
         static void Hud()
         {
-            Console.WriteLine("=====================================================");
-            Console.WriteLine("Health: "+ health +"Gold: "+ gold);
+            Console.WriteLine();
+            Console.WriteLine("[]===================================================[]");
+            Console.WriteLine("                      - TURN "+turn+" -                ");
+            Console.WriteLine("[]===================================================[]");
+            Console.WriteLine("||                                                   ||");
+            Console.WriteLine("||                                                   ||");
+            Console.WriteLine("||                                                   ||");
+            Console.WriteLine("||                                                   ||");
+            Console.WriteLine("||                                                   ||");
+            Console.WriteLine("||                                                   ||");
+            Console.WriteLine("||                                                   ||");
+            Console.WriteLine("||                                                   ||");
+            Console.WriteLine("[]===================================================[]");
+            Console.WriteLine("         | "+health+" HEALTH | "+gold+" GOLD | "+potion+" POTION |");
+            Console.WriteLine("[]===================================================[]");
+            turn = turn + 1;
+            while (Console.ReadKey(true).key != ConsoleKey.Spacebar);
         }
     }
 }
