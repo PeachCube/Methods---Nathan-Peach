d0ca9e5751ad56464454f76efa17644540d4c664
PeachCube <nathanpeach500@live.com>
2023-09-20 15:01:37 -0300

    Mostly finished. I might reuse some of this code  in future projects in this class.

--- Methods - Nathan Peach/Program.cs
+++ Methods - Nathan Peach/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,6 @@ namespace Methods___Nathan_Peach
 {
     internal class Program
     {
-        //Variables must be static in order for them to be accessed by static methods
         //Variable declaration
         static int health = 100;
         static int gold = 0;
@@ -16,30 +16,48 @@ namespace Methods___Nathan_Peach
         static int potion = 1;
         //Variable declaration
 
-
-
-
-
-
-
+        //Variables must be static in order for them to be accessed by static methods
+        //Methods are written in light yellow
         //Program always reads and executes "Main" first
         static void Main(string[] args)
         {
             //Calls the "Hud" method to print the Hud
             Hud();
+
+            //I'd roll rng here to determine which event runs but I cant out how to get rng.Next to give a usable integer value
+            EventLog();
             TakeDamage(25);
+            
             Hud();
 
+            EventLog();
+            AddScore(50);
+
+            Hud();
         }
+        static void AddScore(int score)
+        {
+            gold = gold + score;
+            Console.WriteLine(score + " GOLD OBTAINED");
+        }       
         static void TakeDamage(int damage) 
         {
             health = health - damage;
+            Console.WriteLine(damage + " DAMAGE TAKEN");
+        }
+        static void EventLog()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("---------   |");
+            Console.WriteLine("EVENT LOG   |");
+            Console.WriteLine("---------   V");
+            Console.WriteLine("");
         }
         static void Hud()
         {
             Console.WriteLine();
             Console.WriteLine("[]===================================================[]");
-            Console.WriteLine("                      - TURN "+turn+" -                ");
+            Console.WriteLine("                      - TURN " + turn + " -                ");
             Console.WriteLine("[]===================================================[]");
             Console.WriteLine("||                                                   ||");
             Console.WriteLine("||                                                   ||");
@@ -50,10 +68,14 @@ namespace Methods___Nathan_Peach
             Console.WriteLine("||                                                   ||");
             Console.WriteLine("||                                                   ||");
             Console.WriteLine("[]===================================================[]");
-            Console.WriteLine("         | "+health+" HEALTH | "+gold+" GOLD | "+potion+" POTION |");
+            Console.WriteLine("         | " + health + " HEALTH | " + gold + " GOLD | " + potion + " POTION |");
             Console.WriteLine("[]===================================================[]");
+            Console.WriteLine("      -PRESS SPACEBAR TO ADVANCE TO NEXT TURN-");
             turn = turn + 1;
-            while (Console.ReadKey(true).key != ConsoleKey.Spacebar);
+            while (Console.ReadKey(true).Key != ConsoleKey.Spacebar)
+            {
+            
+            }
         }
     }
 }
